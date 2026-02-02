7174fa9efff535a3b591b009b74e0c6abbe61fbe
Brittany Scott <brittanyscott@Mac.lan>
2026-02-01 21:21:08 -0500

    tested js addition with console log. successful

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("Js is connected");
+function smoothScroll() {
+  const navLinks = document.querySelectorAll("nav a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const navHash = document.querySelector(link.hash);
+      navHash.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+}
+
+smoothScroll();
